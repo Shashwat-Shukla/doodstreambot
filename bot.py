ce5125c7a241133e6ed2096e810cf06086bad31e
Shashwat-Shukla <44372206+Shashwat-Shukla@users.noreply.github.com>
2022-01-31 12:42:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,9 +106,7 @@ async def Doodstream_up(link):
     title_new = urlparse(link)
     title_new = os.path.basename(title_new.path)
     title_Doodstream = '@' + CHANNEL + title_new
-    realaurl = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
-    param = {'token':f'{MDISK_TOKEN}','link':link}
-    res = requests.post(realaurl, json = param)         
+    res = requests.get('https://urlshortx.com/api?api=3b471a00838ea35956397d2b074fad7cdea8a5fe&url=' + link)
     data = res.json()
     data = dict(data)
     print(data)
