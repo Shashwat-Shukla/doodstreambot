b8ab18860ef28c79bc5d57425c7ae8567514e271
Shashwat-Shukla <44372206+Shashwat-Shukla@users.noreply.github.com>
2022-02-01 10:08:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,10 +151,9 @@ async def new_Doodstream_url(urls):
 
 
 async def remove_username(new_List):
-    for i in range(len(new_List)):
+    for i in new_List:
         if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i):
-           if new_List[i] == '@': 
-              new_List[i] = 'Shardul'
+            new_List.remove(i)
     return new_List
 
 async def addFooter(str):
